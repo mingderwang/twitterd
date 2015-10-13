c67006d4f10f77e17d8ce09560bfa903b856a164
Ming-der Wang <ming@log4analytics.com>
2015-10-13 18:04:47 +0800

    new lib, good for usersteam now

--- main.go
+++ main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"fmt"
-	"github.com/ChimeraCoder/anaconda"
+	"github.com/k0kubun/twitter"
+	"github.com/k0kubun/userstream"
 	"os"
-	"time"
 )
 
 func main() {
@@ -12,14 +12,43 @@ func main() {
 	var CONSUMER_SECRET = os.Getenv("CONSUMER_SECRET")
 	var ACCESS_TOKEN = os.Getenv("ACCESS_TOKEN")
 	var ACCESS_TOKEN_SECRET = os.Getenv("ACCESS_TOKEN_SECRET")
-	anaconda.SetConsumerKey(CONSUMER_KEY)
-	anaconda.SetConsumerSecret(CONSUMER_SECRET)
-	api := anaconda.NewTwitterApi(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-	fmt.Println(*api.Credentials)
-	api.EnableThrottling(1*time.Second, 5)
-
-	searchResult, _ := api.DirectMessages(nil)
-	for _, tweet := range searchResult.Statuses {
-		fmt.Print(tweet.Text)
+	client := &userstream.Client{
+		ConsumerKey:       CONSUMER_KEY,
+		ConsumerSecret:    CONSUMER_SECRET,
+		AccessToken:       ACCESS_TOKEN,
+		AccessTokenSecret: ACCESS_TOKEN_SECRET,
 	}
+
+	client.UserStream(func(event interface{}) {
+		switch event.(type) {
+		case *twitter.Tweet:
+			tweet := event.(*twitter.Tweet)
+			fmt.Printf("%s: %s\n", tweet.User.ScreenName, tweet.Text)
+		case *userstream.Delete:
+			tweetDelete := event.(*userstream.Delete)
+			fmt.Printf("[delete] %d\n", tweetDelete.Id)
+		case *userstream.Favorite:
+			favorite := event.(*userstream.Favorite)
+			fmt.Printf("[favorite] %s => %s : %s\n",
+				favorite.Source.ScreenName, favorite.Target.ScreenName, favorite.TargetObject.Text)
+		case *userstream.Unfavorite:
+			unfavorite := event.(*userstream.Unfavorite)
+			fmt.Printf("[unfavorite] %s => %s : %s\n",
+				unfavorite.Source.ScreenName, unfavorite.Target.ScreenName, unfavorite.TargetObject.Text)
+		case *userstream.Follow:
+			follow := event.(*userstream.Follow)
+			fmt.Printf("[follow] %s => %s\n", follow.Source.ScreenName, follow.Target.ScreenName)
+		case *userstream.Unfollow:
+			unfollow := event.(*userstream.Unfollow)
+			fmt.Printf("[unfollow] %s => %s\n", unfollow.Source.ScreenName, unfollow.Target.ScreenName)
+		case *userstream.ListMemberAdded:
+			listMemberAdded := event.(*userstream.ListMemberAdded)
+			fmt.Printf("[list_member_added] %s (%s)\n",
+				listMemberAdded.TargetObject.FullName, listMemberAdded.TargetObject.Description)
+		case *userstream.ListMemberRemoved:
+			listMemberRemoved := event.(*userstream.ListMemberRemoved)
+			fmt.Printf("[list_member_removed] %s (%s)\n",
+				listMemberRemoved.TargetObject.FullName, listMemberRemoved.TargetObject.Description)
+		}
+	})
 }
