9efbd07ecb3e06478d175f8faa91f463d864a2d0
Ming-der Wang <ming@log4analytics.com>
2015-10-13 17:13:05 +0800

    format

--- main.go
+++ main.go
@@ -2,24 +2,24 @@ package main
 
 import (
 	"fmt"
+	"github.com/ChimeraCoder/anaconda"
 	"os"
 	"time"
-	"github.com/ChimeraCoder/anaconda"
 )
 
 func main() {
-var CONSUMER_KEY = os.Getenv("CONSUMER_KEY")
-var CONSUMER_SECRET = os.Getenv("CONSUMER_SECRET")
-var ACCESS_TOKEN = os.Getenv("ACCESS_TOKEN")
-var ACCESS_TOKEN_SECRET = os.Getenv("ACCESS_TOKEN_SECRET")
+	var CONSUMER_KEY = os.Getenv("CONSUMER_KEY")
+	var CONSUMER_SECRET = os.Getenv("CONSUMER_SECRET")
+	var ACCESS_TOKEN = os.Getenv("ACCESS_TOKEN")
+	var ACCESS_TOKEN_SECRET = os.Getenv("ACCESS_TOKEN_SECRET")
 	anaconda.SetConsumerKey(CONSUMER_KEY)
 	anaconda.SetConsumerSecret(CONSUMER_SECRET)
 	api := anaconda.NewTwitterApi(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 	fmt.Println(*api.Credentials)
 	api.EnableThrottling(1*time.Second, 5)
 
-searchResult, _ := api.DirectMessages(nil)
-for _ , tweet := range searchResult.Statuses {
-    fmt.Print(tweet.Text)
-}
+	searchResult, _ := api.DirectMessages(nil)
+	for _, tweet := range searchResult.Statuses {
+		fmt.Print(tweet.Text)
+	}
 }
